5e526e86426619b5ea9535bf280c4c4f3bc7d77f
rinziv <rinzivillo@isti.cnr.it>
2021-03-26 17:34:58 +0100

    Optimization of code and cascading selections

--- src/App.vue
+++ src/App.vue
@@ -14,46 +14,67 @@ export default {
   components: {
 
   },
+  data(){
+    return {
+      numbers: [100,250,160,80, 200],
+    }
+  },
+
   mounted(){
-    let numbers = [100,250,160,80, 200, 300, 120, 240, 1000];
+    this.refreshChart(this.numbers);
+  },
 
-    // select visual enviroment: SvG
-    const svg = d3.select('#viz');
+  watch:{
+    numbers(newVal){
+      this.refreshChart(newVal);
+    }
+  },
+  methods: {
+    refreshChart(listOfNumbers){
+      // select visual enviroment: SvG
+      const svg = d3.select('#viz');
 
-    const scaleLength = d3.scaleLinear()
-            .domain([0, d3.max(numbers)])
-            .range([0, 600]);
-    const lAxis = d3.axisTop(scaleLength);
+      const scaleLength = d3.scaleLinear()
+              .domain([0, d3.max(listOfNumbers)])
+              .range([0, 600]);
+      const lAxis = d3.axisTop(scaleLength);
 
-    const scalePos = d3.scaleBand()
-            .domain(d3.range(numbers.length))
-            .range([0, 300])
-            .round(true)
-            .paddingInner(0.05)
-            .paddingOuter(0.05);
+      const scalePos = d3.scaleBand()
+              .domain(d3.range(listOfNumbers.length))
+              .range([0, 300])
+              .round(true)
+              .paddingInner(0.05)
+              .paddingOuter(0.05);
 
-    // =============== Create g groups ===============
-    svg.append('g')
-      .attr('class','lAxis')
-      .attr('transform', 'translate(20, 20)')
-      .call(lAxis);
+      // =============== Create g groups ===============
+      svg.selectAll('g.lAxis')
+              .data([0])
+              .join('g')
+              .attr('class','lAxis')
+              .attr('transform', 'translate(20, 20)')
+              .call(lAxis);
 
-    const gs = svg.selectAll('g.bars')
-      .data(numbers)
-      .join('g').attr('class','bars');
+      const gs = svg.selectAll('g.bars')
+              .data(listOfNumbers)
+              .join('g').attr('class','bars');
 
-    gs.attr('transform', (d, i) => `translate(20, ${30 + scalePos(i)})`);
+      gs.attr('transform', (d, i) => `translate(20, ${30 + scalePos(i)})`);
 
-    gs.append('rect')
-            .attr('fill', '#0a8989')
-            .attr('height', scalePos.bandwidth())
-            .attr('width', scaleLength );
+      gs.selectAll('rect')
+           .data(d => [d])
+           .join('rect')
+              .attr('fill', '#0a8989')
+              .attr('height', scalePos.bandwidth())
+              .attr('width', scaleLength );
 
-    gs.append('text')
-            .text( (d) => d )
-            .attr('x', scaleLength)
-            .attr('y', scalePos.bandwidth() / 2 );
-  }
+      gs.selectAll('text')
+              .data(d => [d])
+              .join('text')
+              .text( (d) => d )
+              .attr('x', scaleLength)
+              .attr('y', scalePos.bandwidth() / 2 );
+    }
+  },
 }
 </script>
 
